d4635754bf5f0300a4fb22c1fac2768e3c7120e6
JuanesVpcb <je.vasquez2@uniandes.edu.co>
2023-10-11 14:18:42 -0500

    Store finished

--- src/Model/Store.java
+++ src/Model/Store.java
@@ -34,9 +34,42 @@ public class Store {
 
     public boolean checkOpeningHours(Calendar current) {
 
-        boolean truthValue = false;
+        boolean truthValue = true;
         
-        // TODO: Finish method
+        int opHour = openingTime.get(Calendar.HOUR);
+        int opMin = openingTime.get(Calendar.MINUTE) + opHour * 60;
+        int clHour = closingTime.get(Calendar.HOUR);
+        int clMin = closingTime.get(Calendar.MINUTE) + clHour * 60;
+        int hour = current.get(Calendar.HOUR);
+        int min = current.get(Calendar.MINUTE) + hour * 60;
+        if (opMin > min || min > clMin) truthValue = false;
+
+        int week = current.get(Calendar.DAY_OF_WEEK);
+        switch (week) {
+
+            case Calendar.MONDAY:
+                if ((openingDays & 1) != 1) truthValue = false;
+                break;
+            case Calendar.TUESDAY:
+                if ((openingDays & 2) != 2) truthValue = false;
+                break;
+            case Calendar.WEDNESDAY:
+                if ((openingDays & 4) != 4) truthValue = false;
+                break;
+            case Calendar.THURSDAY:
+                if ((openingDays & 8) != 8) truthValue = false;
+                break;
+            case Calendar.FRIDAY:
+                if ((openingDays & 16) != 16) truthValue = false;
+                break;
+            case Calendar.SATURDAY:
+                if ((openingDays & 32) != 32) truthValue = false;
+                break;
+            case Calendar.SUNDAY:
+                if ((openingDays & 64) != 64) truthValue = false;
+                break;
+
+        }
 
         return truthValue;
 
