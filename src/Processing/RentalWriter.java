4289388ac0ddc232fd81fbf0cc2646e433d8d82d
Nicotorres7 <n.torresp2@uniandes.edu.co>
2023-10-15 20:18:42 -0500

    RentalWriter NewRental

--- src/Processing/RentalWriter.java
+++ src/Processing/RentalWriter.java
@@ -8,13 +8,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import Model.Client;
+import Model.Insurance;
 import Model.Licence;
 import Model.Payment;
+import Model.Rental;
 
 public class RentalWriter {
+
 
     public static void changeTariffs(HashMap<String, Integer> tariffs) {
         // TODO: Finish implementing the method
@@ -87,6 +91,57 @@ public class RentalWriter {
 
             }
         }
+    }
+    public void newRental(Rental rental){
+        String plate = rental.getCar().getPlate();
+        String folderPath = "data/rentals/" + plate;
+
+        File folder = new File(folderPath);
+
+        if (!folder.exists()) {
+            boolean result = folder.mkdirs();
+            if (result) {
+                String filePath = folderPath + "/info.txt";
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd:HH-mm");
+                String datePickString = dateFormat.format(rental.getPickUp().getTime());
+                String dateReturnString = dateFormat.format(rental.getReturn().getTime());
+                String content = rental.getClient().getLogin() + ',' + rental.getCar().getPlate() + ',' + String.valueOf(rental.getFinalCharge()) + ',' + rental.getOrigin() + ',' + rental.getDestination() + ',' + datePickString + ',' + dateReturnString;
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true))) {
+                    bufferedWriter.append(content);
+                    bufferedWriter.newLine();
+                    System.out.println("\nInformación del usuario guardada con éxito.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String filePath2 = folderPath + "/insurance.txt";
+                ArrayList<Insurance> insurances = rental.getInsurances();
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath2, true))) {
+                    for (Insurance insurance : insurances) {
+                    bufferedWriter.append(insurance.getName());
+                    bufferedWriter.newLine();
+                    }
+                } 
+                
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String filePath3 = folderPath + "/insurance.txt";
+                ArrayList<Licence> secondaryLicences = rental.getSecondaryDriver();
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath3, true))) {
+                    for (Licence lic : secondaryLicences) {
+                    bufferedWriter.append(String.valueOf(lic.getNumber()));
+                    bufferedWriter.newLine();
+                    }
+                } 
+                
+                catch (IOException e) {
+                    e.printStackTrace();
+                }    
+            }
+        }
+
+                
+
     }
 }
 
